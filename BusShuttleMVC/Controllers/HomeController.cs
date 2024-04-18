178068f2c95e411c12f5d9ef8d516d9337c5f2e4
North-Kaster <bckaster@bsu.edu>
2024-04-18 16:03:39 -0400

    Add initial files

---
+++ BusShuttleMVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using BusShuttleMVC.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BusShuttleMVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+    [Authorize(Policy = "IsManager")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [Authorize(Policy = "isDriver")]
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
