afc2f1b245d479433a9f71906759ad0714872bf6
North-Kaster <bckaster@bsu.edu>
2024-04-24 23:38:31 -0400

    Cleanup views

--- DomainModel/obj/Debug/net8.0/DomainModel.AssemblyInfo.cs
+++ DomainModel/obj/Debug/net8.0/DomainModel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DomainModel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d24d41359590678a0ad2ca48abf538752f5bb05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f011ca63b81cedc6a72a450838904e2161bcca2")]
 [assembly: System.Reflection.AssemblyProductAttribute("DomainModel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DomainModel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
